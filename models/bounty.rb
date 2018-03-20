b1e3bbb7f22f0a09c2e5cf3ac37235b4cd0b92fc
Claire <cconnachan@gmail.com>
2018-03-20 16:00:25 +0000

    added bounty class for ruby, added save_to_db method

--- models/bounty.rb
+++ models/bounty.rb
@@ -3,6 +3,50 @@ require('pg')
 
 class Bounty
 
+attr_reader :id
+attr_accessor :name, :species, :bounty_value, :danger_level
+
+  def initialize( bounty_info )
+
+    @name = bounty_info['name']
+    @species = bounty_info['species']
+    @bounty_value = bounty_info['bounty_value'].to_i
+    @danger_level = bounty_info['danger_level']
+    @id = bounty_info['id']
+
+  end
+
+  def save_to_db()
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost' } )
+
+    sql = "
+    INSERT INTO bounties
+      (name, species, bounty_value, danger_level)
+      VALUES
+      ($1, $2, $3, $4);
+    "
+
+    values = [@name, @species, @bounty_value, @danger_level]
+
+    db.prepare("save", sql)
+    db.exec_prepared("save", values)
+
+  end
+
+  def update()
+
+  end
+
+  def delete()
+
+  end
 
 
 end
+
+
+
+# Create a Ruby file for your Bounty class - Bounty
+# Write your class definition in the file - initialize, attr_readers, instance variables
+
+# Implement save, update and delete methods on your class
--- runner.rb
+++ runner.rb
@@ -1,2 +1,13 @@
 require('pry')
 require_relative('models/bounty.rb')
+
+first_target = { 'name' => 'Boba Fett', 'species' => 'human', 'bounty_value' => '750', 'danger_level' => 'red alert' }
+
+my_bounty = Bounty.new(first_target)
+
+second_target = { 'name' => 'Willy', 'species' => 'killer whale', 'bounty_value' => '25', 'danger_level' => 'low' }
+
+my_other_bounty = Bounty.new(second_target)
+
+my_bounty.save_to_db()
+my_other_bounty.save_to_db()
